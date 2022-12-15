08d17819cdf387e8724ee4292074d0a336884a0f
GJYyyyy <770248440@qq.com>
2022-12-15 17:59:51 +0800

    deploy

--- scripts/971.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[971],{2971:(n,t,e)=>{e.r(t),e.d(t,{default:()=>s});var l=function(){return(0,this._self._c)("div",[this._v("\n    收藏列表\n")])};l._withStripped=!0;const s=(0,e(1900).Z)({},l,[],!1,null,null,null).exports}}]);
